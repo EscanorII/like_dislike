60e99cffa0d0cef269261c5b8d88ae25aa374363
EscanorII <ivanborovyi@gmail.com>
2021-07-06 22:21:00 +0300

    change footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import style from "./Footer.module.css"
 
 const Footer = () => {
     return (
-        <footer className={style.footer}>Footer</footer>
+        <footer className={style.footer}><h1>Footer1</h1>Footer</footer>
     )
 }
 
